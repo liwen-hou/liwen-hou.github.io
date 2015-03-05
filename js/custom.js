b721a9a9c60e210010a62013051290319591b5e5
Hou Liwen <liwen_zhe@Hous-MacBook-Air-2.local>
2015-03-05 23:15:24 +0800

    update website

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
-    
-    $('.welcome').fadeOut(2000, function(){
+    //welcome page effects
+    $('.quote').fadeIn(1000).delay(4000).fadeOut(1000);
+    $('.welcome').delay(4000).fadeOut(1000, function(){
         $('.main-body').fadeIn(2000);
     });
+    //facebook share api
+    (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+                    fjs.parentNode.insertBefore(js, fjs);
+                    }(document, 'script', 'facebook-jssdk'));
     
+    //twitter sahre api
+    window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||        {};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=  [];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));
+    
+    //scroll screen up by 50px to avoiding covering by the navbar
     window.addEventListener("hashchange", function () { scrollBy(0, -50) })
     
+    //form validation
     $('#send-email').click(function () {
         // Temp store of values
         var email = $("#inputEmail").val();
@@ -13,7 +27,7 @@ $(document).ready(function() {
         // Regex for email validation
         var validateEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
 
-        // Validation: Exception cases
+        // Validation: Fail
         if (!validateEmail.test(email)) {
             $('#inputEmail').attr('data-original-title', 'Please fill in a valid email address');
             $('#inputEmail').tooltip('show');
